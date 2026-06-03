7e7c09bd845c342eb8c15eb5ea45320ea2936bd7
Vinícius Hoyer <contact@vhoyer.dev>
2026-06-03 15:18:32 -0300

    📈 Add google analytics to track usage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,12 @@ export default defineNuxtConfig({
           href: 'https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,700;0,900;1,700&family=IBM+Plex+Mono:wght@400;500&family=Quicksand:wght@300;400;500;600;700&display=swap',
         },
       ],
+      script: [
+        { src: 'https://www.googletagmanager.com/gtag/js?id=G-5VZYSXHDG0', async: true },
+        {
+          innerHTML: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date());gtag('config','G-5VZYSXHDG0');`,
+        },
+      ],
     },
   },
 })
