8bd9b374b5ef5387c489a142d4e7fb762e7f665c
Vinícius Hoyer <contact@vhoyer.dev>
2026-06-03 16:18:55 -0300

    ✨ Add a proper favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,8 @@ export default defineNuxtConfig({
   app: {
     head: {
       link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
         {
